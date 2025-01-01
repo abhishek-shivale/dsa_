8d606129d7acfe036e2beac9b30d0c8af387de1a
abhishek-shivale <abhishekshivale45@gmail.com>
2025-01-01 20:22:50 +0530

    as

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,56 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+
+
+// int main() {
+//    int T;
+//    cin >> T;
+//    while(T--) {
+//       string s;
+//       cin >> s;
+
+//       int A[26] = {0};
+//       for(int i = 0; i < s.size(); i++) {
+//          A[s[i] - 'a']++;
+//       }
+//       int pass = false;
+//       for(int i = 0; i < 26; i++) {
+//          if(A[i] > 1){
+//             pass = true;
+//             break;
+//          }
+//       }
+//       if(pass) {
+//          cout << "Yes" << endl;
+//       } else {
+//          cout << "No" << endl;
+//       }
+
+//    }
+// }
+
+
 int main() {
-  int n;
-  cin >> n;
-  cout << n << endl;
-  return 0;
+   int T;
+   cin >> T;
+   int constest = 0;
+   while(T--) {
+      int A[3] = {0};
+      int count = 0;
+      for(int i = 0; i < 3; i++) {
+         cin >> A[i];
+         if(A[i] == 1) {
+            count++;
+         }
+      }
+      if(count > 1) {
+         constest++;
+      }
+   }
+      cout << constest << endl;
 }
+
+
+
